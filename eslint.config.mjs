8387ef23ba472f2966d30f5f11bbdaa49d6a7ea0
Lexi Robinson <lexi@lexi.org.uk>
2024-10-07 12:50:10 +0100

    Update everything
    I wish this wasn't so painful, god

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            ecmaVersion: 2022, // Guessed, based on node.green
+            sourceType: "commonjs", // For now
+        },
+
+        rules: {
+            "prefer-const": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    eslintConfigPrettier,
+);
